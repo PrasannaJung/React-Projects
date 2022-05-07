11e4d0397db12f885cf3cb699876410b8b71d810
PrasannaJung <prasannajung08@gmail.com>
2022-05-07 22:49:04 +0545

    Fix typo

--- expense-tracker/src/components/Money.js
+++ expense-tracker/src/components/Money.js
@@ -5,7 +5,7 @@ const Money = () => {
     <div className='max-w-xl bg-slate-900 p-6 ml-4 rounded-md mb-4 text-white'>
       <div className='flex justify-between'>
         <div className='flex flex-col items-center'>
-          <p>Total Earnings</p>
+          <p>Total Budget</p>
           <p>Rs.10000</p>
         </div>
         <div className='flex flex-col items-center'>
@@ -14,7 +14,7 @@ const Money = () => {
         </div>
         <div className='flex flex-col items-center'>
           <p>Amount Left</p>
-          <p>Rs.5000</p>
+          <p>Rs.7500</p>
         </div>
       </div>
     </div>
