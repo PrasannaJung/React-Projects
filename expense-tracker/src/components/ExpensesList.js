81285d2a74d5691f022fff43baa0a6d88fb38a15
PrasannaJung <prasannajung08@gmail.com>
2022-05-08 19:13:41 +0545

    add keys in the list

--- expense-tracker/src/components/ExpensesList.js
+++ expense-tracker/src/components/ExpensesList.js
@@ -1,12 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const ExpensesList = props => {
   return (
-    <div className='bg-slate-900 flex-1 p-3 rounded-sm'>
+    <div className='bg-slate-900 flex-1 p-3 rounded-sm text-white'>
       <ul className='flex flex-col gap-4'>
         {props.myExpenses.map(expense => {
           return (
-            <li className='flex justify-between bg-slate-400 py-1 px-2'>
+            <li
+              key={props.myExpenses.indexOf(expense)}
+              className='flex justify-between bg-gray-600 py-1 px-2'
+            >
               <p>{expense.expenseDate.toString()}</p>
               <p>{expense.expense}</p>
               <p>Rs {expense.amount}</p>
