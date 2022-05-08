0933243227ff4eb6c252aa6b4bcaabfa06966809
PrasannaJung <prasannajung08@gmail.com>
2022-05-08 22:47:48 +0545

    add progress bar

--- expense-tracker/src/components/Money.js
+++ expense-tracker/src/components/Money.js
@@ -3,6 +3,15 @@ import React from 'react';
 const Money = () => {
   return (
     <div className='max-w-xl bg-slate-900 p-6 ml-4 rounded-md mb-4 text-white'>
+      <div className='flex justify-center items-center flex-col'>
+        <progress className='rounded-md mt-3' id='money' value='32' max='100'>
+          {' '}
+          32%{' '}
+        </progress>
+        <label className='mt-1 mb-3' htmlFor='money'>
+          Money
+        </label>
+      </div>
       <div className='flex justify-between'>
         <div className='flex flex-col items-center'>
           <p>Total Budget</p>
