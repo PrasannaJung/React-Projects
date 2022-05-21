6ef538585225b7cbd2443ee4d50fd2da4fd0344e
PrasannaJung <prasannajung08@gmail.com>
2022-05-21 21:15:14 +0545

    ui and little functionalities add

--- birthday-reminder-app/src/App.js
+++ birthday-reminder-app/src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
 import List from './List';
 import data from './data';
 
 function App() {
-  return;
+  const [people, setPeople] = useState(data);
+
+  const clearPeople = () => {
+    setPeople([]);
+  };
+  return (
+    <main>
+      <section className='container'>
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={clearPeople}>Clear All</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
--- birthday-reminder-app/src/List.js
+++ birthday-reminder-app/src/List.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
-const List = () => {
+const List = props => {
   return (
     <>
-      <h2>list component</h2>
+      {props.people.map(person => {
+        const { id, name, age, image } = person;
+
+        return (
+          <article key={id} className='person'>
+            <img src={image} alt={name} />
+            <div>
+              <h3>{name}</h3>
+              <p>{age} years</p>
+            </div>
+          </article>
+        );
+      })}
     </>
   );
 };
