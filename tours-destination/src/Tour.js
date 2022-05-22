a8b3377f45462efa483bf706bcffc5e8b885af32
PrasannaJung <prasannajung08@gmail.com>
2022-05-22 18:37:06 +0545

    Add functionalities

--- tours-destination/src/Tour.js
+++ tours-destination/src/Tour.js
@@ -1,7 +1,25 @@
 import React, { useState } from 'react';
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = props => {
+  const [readMore, setReadMore] = useState(false);
+  return (
+    <article className='single-tour'>
+      <img src={props.image} alt={props.name} />
+      <footer>
+        <div className='tour-info'>
+          <h4>{props.name}</h4>
+          <h4 className='tour-price'>${props.price}</h4>
+        </div>
+        <p>
+          {readMore ? props.info : `${props.info.substring(0, 200)}...`}
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? 'Show less' : 'Show more'}
+          </button>
+        </p>
+        <button className='delete-btn'>Not Interested</button>
+      </footer>
+    </article>
+  );
 };
 
 export default Tour;
