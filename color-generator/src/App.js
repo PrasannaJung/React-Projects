2c8058a42fa58ebee038c3fa1d6613edb0a7e811
PrasannaJung <prasannajung08@gmail.com>
2022-06-09 18:24:28 +0545

    UI components done

--- color-generator/src/App.js
+++ color-generator/src/App.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import SingleColor from './SingleColor';
 
-function App() {
-  return <div className='App'></div>;
-}
+import Values from 'values.js';
+
+const App = () => {
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log('Hello');
+  };
+
+  return (
+    <>
+      <section className='container'>
+        <h3>Color generator</h3>
+        <form action='' onSubmit={handleSubmit}>
+          <input
+            type='text'
+            value={color}
+            onChange={e => setColor(e.target.value)}
+            placeholder='#f15025'
+          />
+          <button className='btn' type='submit'>
+            Submit
+          </button>
+        </form>
+      </section>
+      <section className='colors'>
+        <h4>List goes here</h4>
+      </section>
+    </>
+  );
+};
 
 export default App;
