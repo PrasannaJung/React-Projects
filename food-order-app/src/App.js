ed2c3616724e135dcc0d599d471df93e9332ad6c
PrasannaJung <prasannajung08@gmail.com>
2022-08-22 10:29:24 +0545

    Initial project setup

---
+++ food-order-app/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Navigation from './components/Header/Navigation';
+
+function App() {
+  return <></>;
+}
+
+export default App;
---
+++ food-order-app/src/components/Header/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navigation = () => {
+  return (
+    <header className='navbar'>
+      <h1>DineDelicious</h1>
+      <div>
+        <button>Show My Orders</button>
+      </div>
+    </header>
+  );
+};
+
+export default Navigation;
